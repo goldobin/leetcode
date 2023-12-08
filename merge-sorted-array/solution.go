940d8f42013fc861ad7883b793a6f1f0dcff4b9b
Oleksandr Goldobin <alex.goldobin@gmail.com>
2023-12-08 14:20:17 +0100

    Implement Merge Sorted Array

---
+++ merge-sorted-array/solution.go
@@ -0,0 +1,52 @@
+package merge_sorted_array
+
+import (
+	"fmt"
+)
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	if len(nums1) != m+n {
+		panic(fmt.Sprintf("the lengt of nums1 should be %d", m+n))
+	}
+
+	if n == 0 {
+		return
+	}
+
+	if m == 0 {
+		for j := 0; j < n; j++ {
+			nums1[j] = nums2[j]
+		}
+	}
+
+	i := m - 1
+	j := n - 1
+	k := len(nums1) - 1
+	for {
+		if k < 0 || j < 0 {
+			break
+		}
+
+		if i < 0 {
+			for n := 0; n <= j; n++ {
+				nums1[n] = nums2[n]
+			}
+			break
+		}
+
+		if nums1[i] > nums2[j] {
+			nums1[k] = nums1[i]
+			i--
+		} else if nums1[i] < nums2[j] {
+			nums1[k] = nums2[j]
+			j--
+		} else {
+			nums1[k] = nums2[j]
+			j--
+			k--
+			nums1[k] = nums1[i]
+			i--
+		}
+		k--
+	}
+}
---
+++ merge-sorted-array/solution_test.go
@@ -0,0 +1,91 @@
+package merge_sorted_array
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func Test_merge(t *testing.T) {
+	cases := []struct {
+		nums1    []int
+		m        int
+		nums2    []int
+		n        int
+		expected []int
+	}{
+		{
+			nums1:    []int{1, 2, 3, 0, 0, 0},
+			m:        3,
+			nums2:    []int{2, 5, 6},
+			n:        3,
+			expected: []int{1, 2, 2, 3, 5, 6},
+		},
+		{
+			nums1:    []int{1, 2, 3, 0, 0, 0},
+			m:        3,
+			nums2:    []int{4, 5, 6},
+			n:        3,
+			expected: []int{1, 2, 3, 4, 5, 6},
+		},
+		{
+			nums1:    []int{4, 5, 6, 0, 0, 0},
+			m:        3,
+			nums2:    []int{1, 2, 3},
+			n:        3,
+			expected: []int{1, 2, 3, 4, 5, 6},
+		},
+		{
+			nums1:    []int{1},
+			m:        1,
+			nums2:    []int{},
+			n:        0,
+			expected: []int{1},
+		},
+
+		{
+			nums1:    []int{0},
+			m:        0,
+			nums2:    []int{1},
+			n:        1,
+			expected: []int{1},
+		},
+		{
+			nums1:    []int{1, 2, 0},
+			m:        2,
+			nums2:    []int{1},
+			n:        1,
+			expected: []int{1, 1, 2},
+		},
+		{
+			nums1:    []int{1, 0, 0},
+			m:        1,
+			nums2:    []int{1, 2},
+			n:        2,
+			expected: []int{1, 1, 2},
+		},
+		{
+			nums1:    []int{1, 0},
+			m:        1,
+			nums2:    []int{1},
+			n:        1,
+			expected: []int{1, 1},
+		},
+	}
+
+	for i, c := range cases {
+		caseName := fmt.Sprintf("Case %02d", i+1)
+		t.Run(caseName, func(t *testing.T) {
+			result := make([]int, len(c.nums1))
+			for i, v := range c.nums1 {
+				result[i] = v
+			}
+
+			merge(result, c.m, c.nums2, c.n)
+
+			if !reflect.DeepEqual(result, c.expected) {
+				t.Errorf("expected=%d, got=%d", c.expected, result)
+			}
+		})
+	}
+}
