f17fed72bdca3bf00c6790ce3be9ac1fafaef0a9
Oleksandr Goldobin <alex.goldobin@gmail.com>
2024-12-27 19:54:47 +0100

    Implement same tree

---
+++ same-tree/solution_test.go
@@ -0,0 +1,83 @@
+package same_tree
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_isSameTree(t *testing.T) {
+	tests := []struct {
+		name string
+		p    *TreeNode
+		q    *TreeNode
+		want bool
+	}{
+		{
+			name: "case 1.1 leetcode",
+			p: &TreeNode{
+				Val: 1,
+				Left: &TreeNode{
+					Val: 2,
+				},
+				Right: &TreeNode{
+					Val: 3,
+				},
+			},
+			q: &TreeNode{
+				Val: 1,
+				Left: &TreeNode{
+					Val: 2,
+				},
+				Right: &TreeNode{
+					Val: 3,
+				},
+			},
+			want: true,
+		},
+		{
+			name: "case 1.2 leetcode",
+			p: &TreeNode{
+				Val: 1,
+				Left: &TreeNode{
+					Val: 2,
+				},
+			},
+			q: &TreeNode{
+				Val: 1,
+				Right: &TreeNode{
+					Val: 3,
+				},
+			},
+			want: false,
+		},
+		{
+			name: "case 1.3 leetcode",
+			p: &TreeNode{
+				Val: 1,
+				Left: &TreeNode{
+					Val: 2,
+				},
+				Right: &TreeNode{
+					Val: 1,
+				},
+			},
+			q: &TreeNode{
+				Val: 1,
+				Left: &TreeNode{
+					Val: 1,
+				},
+				Right: &TreeNode{
+					Val: 2,
+				},
+			},
+			want: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := isSameTree(tt.p, tt.q)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
